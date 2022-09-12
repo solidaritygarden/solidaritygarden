e37966cdbc84618282d96897cdafdb96120a8ae1
comrade_gardener <solidaritygarden@protonmail.com>
2022-09-12 09:32:18 -0500

    fixing some @media styling issues

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
 import styles from '../../styles/Header.module.css';
 
 export default function Header() {
 	return (
 		<nav className={styles.nav}>
-			<Image src="/Grow_it_yourself_Solidarity_edit.jpg" width="300vw" height={300} />
+			{/* <Image src="/Grow_it_yourself_Solidarity_edit.jpg" width="300vw" height={300} /> */}
 			<div>
 				<h1>Solidarity Garden</h1>
 				<p>because lawns suck and solidarity is everything</p>
