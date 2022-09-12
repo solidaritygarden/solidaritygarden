a3dce21a503b34e56c334e24f0fa0629af84e3b6
comrade_gardener <solidaritygarden@protonmail.com>
2022-09-12 15:55:07 -0500

    basic styling of contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import Layout from './components/Layout';
+import styles from '../styles/Contact.module.css';
 
 export default function Contact() {
 	return (
 		<Layout siteTitle={'Contact Us'} siteDescription={'Ways to get in touch with us'}>
-			<p>
-				If you have any questions, comments or concerns, please direct them to the following contact methods, otherwise,
-				use the form below. We are a lean operation, but will respond as soon as time permits.
-			</p>
-			<form>
+			<div id={styles.contactContainer}>
 				<div>
-					<label>Name</label>
-					<input />
+					<p>
+						If you have any questions, comments or concerns, please direct them to the following contact methods,
+						otherwise, use the form below. We are a lean operation, but will respond as soon as time permits.
+					</p>
+					<form id={styles.contactForm}>
+						<div className={styles.input}>
+							<label>Name</label>
+							<input placeholder="Name" />
+						</div>
+						<div className={styles.input}>
+							<label>Email Address</label>
+							<input placeholder="Email Address" />
+						</div>
+						<div className={styles.input}>
+							<label>Comments</label>
+							<input placeholder="Comments" />
+						</div>
+						<button>Submit</button>
+					</form>
 				</div>
-				<div>
-					<label>Email Address</label>
-					<input />
-				</div>
-				<div>
-					<label>Comments</label>
-					<input />
-				</div>
-			</form>
+			</div>
 		</Layout>
 	);
 }
