18290aad3cbe47f11af4f58e8546e924fbf89e95
comrade_gardener <solidaritygarden@protonmail.com>
2022-09-12 23:10:56 -0500

    added cards to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
 import PropTypes from 'prop-types';
 import Layout from './components/Layout';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 // import Image from 'next/image';
 
 const Home = ({ title, description }) => {
 	return (
 		<Layout siteTitle={title} siteDescription={description}>
 			<div className={styles.content}>
-				<section>
-					<h2>Tired: Victory Garden</h2>
-					<h2>Wired: Solidarity Garden</h2>
-					<h3>* grow veggies for yourself and your neighbors</h3>
-					<h3>* ditch your lawn for a sustainable option</h3>
-					<h3>* build local food security</h3>
-					<h3>* </h3>
+				<section id={styles.landingCardSection}>
+					<div className={styles.landingCard}>
+						<Image src="/smallest-jonathan-kemper-1HHrdIoLFpU-unsplash (1).jpg" width="200vw" height="300vh" />
+						<h3>* grow veggies for yourself and your neighbors</h3>
+					</div>
+					<div className={styles.landingCard}>
+						<Image src="/small-valentina-locatelli-P8bsrm8KbM0-unsplash.jpg" width="200vw" height="300vh" />
+						<h3>* ditch your lawn for a sustainable option</h3>
+					</div>
+					<div className={styles.landingCard}>
+						<Image src="/small-priscilla-du-preez-JCZ2pE-Szpw-unsplash.jpg" width="200vw" height="300vh" />
+						<h3>* build local food security</h3>
+					</div>
 				</section>
 			</div>
 		</Layout>
