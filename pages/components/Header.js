8931d9c755ef80d2952db834ba20ecdfdf5ecb0d
comrade_gardener <solidaritygarden@protonmail.com>
2022-09-13 07:14:18 -0500

    header styling updates

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -5,21 +5,23 @@ import styles from '../../styles/Header.module.css';
 
 export default function Header() {
 	return (
-		<nav className={styles.nav}>
+		<nav className={styles.header}>
 			{/* <figure>
 				<Image src="/Grow_it_yourself_Solidarity_edit.jpg" width="300vw" height={300} />
 				<figcaption>This is an image of world war 2 victory garden propaganda</figcaption>
 			</figure> */}
-			<div>
+			<div className={styles.siteTitle}>
 				<Link href="/">
 					<a className={styles.title}>Solidarity Garden</a>
 				</Link>
 				<p className={styles.subtitle}>because lawns suck and solidarity is everything</p>
 			</div>
-			<Link href="/how">How It Works</Link>
-			<Link href="/support">Support</Link>
-			<Link href="/about">About</Link>
-			<Link href="/contact">Contact</Link>
+			<div className={styles.nav}>
+				<Link href="/how">How It Works</Link>
+				<Link href="/support">Support</Link>
+				<Link href="/about">About</Link>
+				<Link href="/contact">Contact</Link>
+			</div>
 		</nav>
 	);
 }
