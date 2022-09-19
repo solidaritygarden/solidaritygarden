e41a26683a087b8e5153a74b22d96e4fc68f1e89
comrade_gardener <solidaritygarden@protonmail.com>
2022-09-19 18:56:39 -0500

    added icon links to footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,12 +1,44 @@
-// import Link from 'next/link';
 import React from 'react';
 import styles from '../../styles/Footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
+			<Link href="https://www.github.com">
+				<figure className={styles.imageWrapper}>
+					<div style={{ height: '2vh', width: '2vw', position: 'relative' }}>
+						<Image src="/GitHub-Mark-Light-120px-plus.png" width={100} height={100} layout="responsive" />
+						{/* <figcaption></figcaption> */}
+					</div>
+				</figure>
+			</Link>
+			<Link href="https://www.google.com">
+				<figure className={styles.imageWrapper}>
+					<div style={{ height: '2vh', width: '2vw', position: 'relative' }}>
+						<Image src="/Twitter social icons - circle - white.png" width={100} height={100} layout="responsive" />
+						{/* <figcaption></figcaption> */}
+					</div>
+				</figure>
+			</Link>
 			<p>Please copy this site!</p>
-			{/* <Link href="#top">Back to top of page</Link> */}
+			<Link href="https://www.opencollective.org">
+				<figure className={styles.imageWrapper}>
+					<div style={{ height: '2vh', width: '2vw', position: 'relative' }}>
+						<Image src="/Twitter social icons - circle - white.png" width={100} height={100} layout="responsive" />
+						{/* <figcaption></figcaption> */}
+					</div>
+				</figure>
+			</Link>
+			<Link href="https://www.twitter.com">
+				<figure className={styles.imageWrapper}>
+					<div style={{ height: '2vh', width: '2vw', position: 'relative' }}>
+						<Image src="/Twitter social icons - circle - white.png" width={100} height={100} layout="responsive" />
+						{/* <figcaption></figcaption> */}
+					</div>
+				</figure>
+			</Link>
 		</footer>
 	);
 }
